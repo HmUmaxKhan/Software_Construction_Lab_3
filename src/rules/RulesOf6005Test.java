adada953e64d85893e018046368c46261b9e59bf
Muhammad Umar Khan <mukhan.bese22seecs@seecs.edu.pk>
2024-09-23 16:08:05 +0500

    Test cases in Junit Repo

--- src/rules/RulesOf6005Test.java
+++ src/rules/RulesOf6005Test.java
@@ -31,5 +31,10 @@ public class RulesOf6005Test {
         // Test case where code is available to others and citing the source is true
         assertTrue(RulesOf6005.mayUseCodeInAssignment(false, true, false, true, false));
     }
+    
+    public void testAvailableToOthersWithoutCitingSource() {
+        // Test case where code is available to others but the source is not cited
+        assertFalse(RulesOf6005.mayUseCodeInAssignment(false, true, false, false, false));
+    }
 
 }
